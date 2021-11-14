1855d6a85f85ebe49a153f459b94b63d70157667
Mostafa Rabea Ela3ser <73349514+MostafaLa3ser@users.noreply.github.com>
2021-11-14 17:43:07 +0200

    Add files via upload

---
+++ OOP Task/3/third.java
@@ -0,0 +1,11 @@
+/**
+ * third
+ */
+public class third {
+
+    public static void main(String[] args) {
+        MyPoint m = new MyPoint(5.0,8.0);
+        m.show();
+        m.distance();
+    }
+}
